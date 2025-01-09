97fa44ece798e9c33adc7f058aec18fcac7e5b50
Angelo Maia <request.angelo@gmail.com>
2025-01-09 16:57:34 -0300

    Add validations and unit tests for Mailbox

--- app/app/models/mailbox.rb
+++ app/app/models/mailbox.rb
@@ -1,3 +1,7 @@
 class Mailbox < ApplicationRecord
   belongs_to :domain
+
+  validates :username, presence: true, uniqueness: { scope: :domain_id }
+  validates :password, presence: true, format: { with: /\A.{8,}\z/ }
+  validates :scheduled_password_expiration, presence: true
 end
--- app/test/models/mailbox_test.rb
+++ app/test/models/mailbox_test.rb
@@ -1,7 +1,56 @@
 require "test_helper"
 
 class MailboxTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @domain = Domain.new(domain_name: "example.com", password_expiration_date: 30)
+  end
+
+  test "should create mailbox with valid attributes" do
+    mailbox = Mailbox.new(domain: @domain, username: "user", password: "password", scheduled_password_expiration: Date.today + 30)
+    assert mailbox.valid?
+  end
+
+  test "should not create mailbox without domain" do
+    mailbox = Mailbox.new(username: "user", password: "password", scheduled_password_expiration: Date.today + 30)
+    assert mailbox.invalid?
+    assert_equal [ "Domain must exist" ], mailbox.errors.full_messages
+  end
+
+  test "should not create mailbox without username" do
+    mailbox = Mailbox.new(domain: @domain, password: "password", scheduled_password_expiration: Date.today + 30)
+    assert mailbox.invalid?
+    assert_equal [ "Username can't be blank" ], mailbox.errors.full_messages
+  end
+
+  test "should not create mailbox without password" do
+    mailbox = Mailbox.new(domain: @domain, username: "user", scheduled_password_expiration: Date.today + 30)
+    assert mailbox.invalid?
+    assert_equal [ "Password can't be blank", "Password is invalid" ], mailbox.errors.full_messages
+  end
+
+  test "should not create mailbox without scheduled_password_expiration" do
+    mailbox = Mailbox.new(domain: @domain, username: "user", password: "password")
+    assert mailbox.invalid?
+    assert_equal [ "Scheduled password expiration can't be blank" ], mailbox.errors.full_messages
+  end
+
+  test "should not create mailbox with duplicate username in the same domain" do
+    Mailbox.create!(domain: @domain, username: "user", password: "password", scheduled_password_expiration: Date.today + 30)
+    mailbox = Mailbox.new(domain: @domain, username: "user", password: "password", scheduled_password_expiration: Date.today + 30)
+    assert mailbox.invalid?
+    assert_equal [ "Username has already been taken" ], mailbox.errors.full_messages
+  end
+
+  test "should allow duplicate username in different domains" do
+    domain2 = Domain.new(domain_name: "example.org", password_expiration_date: 30)
+    Mailbox.create!(domain: @domain, username: "user", password: "password", scheduled_password_expiration: Date.today + 30)
+    mailbox = Mailbox.new(domain: domain2, username: "user", password: "password", scheduled_password_expiration: Date.today + 30)
+    assert mailbox.valid?
+  end
+
+  test "should not create mailbox with invalid password format" do
+    mailbox = Mailbox.new(domain: @domain, username: "user", password: "short", scheduled_password_expiration: Date.today + 30)
+    assert mailbox.invalid?
+    assert_equal [ "Password is invalid" ], mailbox.errors.full_messages
+  end
 end
